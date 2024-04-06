f3617ec5d61a3310c8b863fd43b7be6a4b9c8948
beomsuKim <ta09472@naver.com>
2024-04-06 21:07:35 +0900

    protected Route 활성화

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { getLocalStorage } from '../util/localStorage'
 import { useEffect } from 'react'
 import { User } from '../schema/User'
@@ -14,17 +14,16 @@ export default function ProtectedRoute({
   const user = getLocalStorage('user') as User | null
   const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     message.info('로그인 후 이용이 가능합니다.')
-  //     navigate('/login')
-  //   }
-  // }, [user])
+  useEffect(() => {
+    if (!user) {
+      message.info('로그인 후 이용이 가능합니다.')
+      navigate('/login')
+    }
+  }, [user])
 
-  // // 유저 정보가 없으면 로딩 상태나 빈 컴포넌트를 반환할 수 있습니다.
-  // if (!user) {
-  //   return null
-  // }
+  if (!user) {
+    return null
+  }
 
-  return children // 유저 정보가 있다면 요청된 경로의 컴포넌트를 렌더링
+  return children
 }
